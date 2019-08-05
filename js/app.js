57f3e4de4ee2e45adfb79378c4db93bcb2b5d720
Martin Ring <ring.martin@gmail.com>
2019-08-05 00:50:23 -0300

    close to finishing 'meet expectations'

--- js/app.js
+++ js/app.js
@@ -1,140 +1,32 @@
-var ctx = document.getElementById('traffic').getContext('2d');
-var myChart1 = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
-        datasets: [{
-            label: '',
-            data: [500, 750, 1250, 1000, 1500, 2000, 1500, 1750, 1250, 1750, 2250, 1750, 2250],
-            backgroundColor: ['rgba(115, 119, 191, 0.2)'],
-            borderColor: ['rgba(115, 119, 191, 1)'],
-            borderWidth: 2
-        }]
-    },
-    options: {
-        maintainAspectRatio: false,
-        legend: {
-            display: false,
-        },
-        scales: {
-            xAxes: [{
-                gridLines:{
-                    drawTicks: false,
-                },
-                ticks: {
-                    padding: 20
-                },
-            }],
-            yAxes: [{
-                gridLines:{
-                    drawTicks: false,
-                },
-                ticks: {
-                    min: 500,
-                    max: 2500,
-                    beginAtZero: false,
-                    padding: 20,
-                }
-            }]
-        },
-        elements: {
-            line: {
-                tension: 0
-            },
-            point: {
-                radius: 8,
-                backgroundColor: 'rgba(255, 255, 255, 1)',
-                borderColor: 'rgba(115, 119, 191, 1)',
-            }
-        }
-    }
-});
+//---------------------------------------------
+//VARIABLES
+//---------------------------------------------
+const alertMsg = document.querySelector('.alert-msg');
+const newNotif = document.querySelector('.new-notification');
+const inputUser = document.querySelector('.input--search-user');
+const msgUser = document.querySelector('.textarea--message-user');
+const sendBtn = document.querySelector('.button--send');
+const missingFieldUser = document.querySelector('.warning-msg-user');
+const missingMsg = document.querySelector('.warning-msg-msg');
 
-var ctx2 = document.getElementById('daily-traffic').getContext('2d');
-var myChart2 = new Chart(ctx2, {
-    type: 'bar',
-    data: {
-        labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
-        datasets: [{
-            label: '',
-            data: [75, 110, 170, 120, 230, 200, 140],
-            backgroundColor: [
-                'rgba(115, 119, 191, 1)',
-                'rgba(115, 119, 191, 1)',
-                'rgba(115, 119, 191, 1)',
-                'rgba(115, 119, 191, 1)',
-                'rgba(115, 119, 191, 1)',
-                'rgba(115, 119, 191, 1)',
-                'rgba(115, 119, 191, 1)',
-            ],
-            borderColor: [''],
-            borderWidth: 0
-        }]
-    },
-    options: {
-        maintainAspectRatio: false,
-        legend: {
-            display: false,
-        },
-        scales: {
-            xAxes: [{
-                barThickness: 24,
-                gridLines:{
-                    drawTicks: false,
-                },
-                ticks: {
-                    padding: 10,
-                }
-            }],
-            yAxes: [{
-                gridLines:{
-                    drawTicks: false,
-                },
-                ticks: {
-                    min: 50,
-                    max: 250,
-                    beginAtZero: false,
-                    padding: 20
-                }
-            }]
-        }
-    }
-});
+//---------------------------------------------
+//EVENT LISTENERS
+//---------------------------------------------
 
-var ctx3 = document.getElementById('mobile-users').getContext('2d');
-var myChart3 = new Chart(ctx3, {
-    type: 'doughnut',
-    data: {
-        labels: ['Phones', 'Tablets', 'Desktop'],
-        datasets: [{
-            label: '',
-            data: [3, 5, 12],
-            backgroundColor: [
-                'rgba(116, 177, 191, 1)',
-                'rgba(129, 201, 143, 1)',
-                'rgba(115, 119, 191, 1)',
-            ],
-            borderColor: ['#fff',],
-            borderWidth: 0
-        }]
-    },
-    options: {
-        legend: {
-            position: 'right',
-        },
-        maintainAspectRatio: false,
-        scales: {
-            yAxes: [{
-                gridLines: {
-                    display: false,
-                    lineWidth: 0,
-                },
-                ticks: {
-                    display: false,
-                    beginAtZero: true,
-                    display: false,
-                }
-            }]
+alertMsg.addEventListener('click', e => {
+    alertMsg.style.display = 'none';
+    newNotif.style.display = 'none';
+    }
+)
+
+sendBtn.addEventListener('click', e => {
+    if(inputUser.value == ''){
+        missingFieldUser.classList.remove('hidden');
+        missingFieldUser.classList.add('show');
+        } 
+    if(msgUser.value == ''){
+        missingMsg.classList.remove('hidden');
+        missingMsg.classList.add('show');
         }
     }
-});
+)
---
+++ js/charts.js
@@ -0,0 +1,140 @@
+var ctx = document.getElementById('traffic').getContext('2d');
+var myChart1 = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
+        datasets: [{
+            label: '',
+            data: [500, 750, 1250, 1000, 1500, 2000, 1500, 1750, 1250, 1750, 2250, 1750, 2250],
+            backgroundColor: ['rgba(115, 119, 191, 0.2)'],
+            borderColor: ['rgba(115, 119, 191, 1)'],
+            borderWidth: 2
+        }]
+    },
+    options: {
+        maintainAspectRatio: false,
+        legend: {
+            display: false,
+        },
+        scales: {
+            xAxes: [{
+                gridLines:{
+                    drawTicks: false,
+                },
+                ticks: {
+                    padding: 20
+                },
+            }],
+            yAxes: [{
+                gridLines:{
+                    drawTicks: false,
+                },
+                ticks: {
+                    min: 500,
+                    max: 2500,
+                    beginAtZero: false,
+                    padding: 20,
+                }
+            }]
+        },
+        elements: {
+            line: {
+                tension: 0
+            },
+            point: {
+                radius: 8,
+                backgroundColor: 'rgba(255, 255, 255, 1)',
+                borderColor: 'rgba(115, 119, 191, 1)',
+            }
+        }
+    }
+});
+
+var ctx2 = document.getElementById('daily-traffic').getContext('2d');
+var myChart2 = new Chart(ctx2, {
+    type: 'bar',
+    data: {
+        labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+        datasets: [{
+            label: '',
+            data: [75, 110, 170, 120, 230, 200, 140],
+            backgroundColor: [
+                'rgba(115, 119, 191, 1)',
+                'rgba(115, 119, 191, 1)',
+                'rgba(115, 119, 191, 1)',
+                'rgba(115, 119, 191, 1)',
+                'rgba(115, 119, 191, 1)',
+                'rgba(115, 119, 191, 1)',
+                'rgba(115, 119, 191, 1)',
+            ],
+            borderColor: [''],
+            borderWidth: 0
+        }]
+    },
+    options: {
+        maintainAspectRatio: false,
+        legend: {
+            display: false,
+        },
+        scales: {
+            xAxes: [{
+                barThickness: 24,
+                gridLines:{
+                    drawTicks: false,
+                },
+                ticks: {
+                    padding: 10,
+                }
+            }],
+            yAxes: [{
+                gridLines:{
+                    drawTicks: false,
+                },
+                ticks: {
+                    min: 50,
+                    max: 250,
+                    beginAtZero: false,
+                    padding: 20
+                }
+            }]
+        }
+    }
+});
+
+var ctx3 = document.getElementById('mobile-users').getContext('2d');
+var myChart3 = new Chart(ctx3, {
+    type: 'doughnut',
+    data: {
+        labels: ['Phones', 'Tablets', 'Desktop'],
+        datasets: [{
+            label: '',
+            data: [3, 5, 12],
+            backgroundColor: [
+                'rgba(116, 177, 191, 1)',
+                'rgba(129, 201, 143, 1)',
+                'rgba(115, 119, 191, 1)',
+            ],
+            borderColor: ['#fff',],
+            borderWidth: 0
+        }]
+    },
+    options: {
+        legend: {
+            position: 'right',
+        },
+        maintainAspectRatio: false,
+        scales: {
+            yAxes: [{
+                gridLines: {
+                    display: false,
+                    lineWidth: 0,
+                },
+                ticks: {
+                    display: false,
+                    beginAtZero: true,
+                    display: false,
+                }
+            }]
+        }
+    }
+});
